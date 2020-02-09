398fea5ecd93c30a000c4beff6820615002b7c88
453627768@qq.com <453627768@qq.com>
2020-02-09 17:48:15 +0800

    项目搭建 引入element-ui

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <router-view/>
+    <el-button type="primary">主要按钮</el-button>
+  </div>
+</template>
+ 
+<script>
+export default {
+  name: 'app'
+}
+</script>
+ 
+<style scoped>
+  
+</style>
