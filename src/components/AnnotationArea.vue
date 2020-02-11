0b79d79be928d10b80278e5d32567e4eed27300f
lbczbl <2553486226@qq.com>
2020-02-11 14:27:21 +0800

    内容精度

---
+++ src/components/AnnotationArea.vue
@@ -0,0 +1,138 @@
+<template>
+	<div class="notes">
+   <div class="note_title">注释区域</div>
+	 <div class="note_content">
+		 <div class="explanation">
+			 <div class="explanation_title">{{notesInfo.title}}</div>
+			 <div class="explanation_content">{{notesInfo.content}}</div>
+		 </div>
+		 <el-divider></el-divider>
+		 <div class="links" v-for="(item,index) in linksList" :key="item.id">
+			 <div class="links_title" @click="toLink(index)">{{item.title}}</div>
+			 <div class="links_website">{{item.website}}</div>
+			 <div class="links_content">{{item.content | snippet}}</div>
+			 <el-divider></el-divider>
+		 </div>
+		 <div class="share">分享到：</div>
+		 <div class="relevant_tags">
+			 <div class="tag-group">
+			 <span class="tag-group__title">相关标签：</span>
+			 <el-tag
+				v-for="item in items"
+				:key="item.label"
+				:type="item.type"
+				effect="plain">
+				{{ item.label }}
+			</el-tag>
+			 </div>
+		 </div>
+	 </div>
+	</div>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				notesInfo: {
+					title: '持卡人',
+					content: '所谓持卡人，就是办理卡片时所用证件的主人，比如张三用其身份证在甲银行柜台办理一张银行卡，则这张银行卡的持卡人就是张三，即使这张银行卡由李四拿着，那持卡人仍是张三，不会变成李四。该银行卡的业务办理，均视为张三办理，由此带来的相关义务也由张三承担。'
+				},
+				linksList: [
+					{
+						id:0,
+						title: '信用卡丢了怎么补办？',
+						website: 'https://zhidao.baidu.com',
+						content: '持卡人应即时办理挂失。信用卡挂失后的盗用风险由银行承担，挂失前48小时内的非凭密码消费对河涸海干'
+					},
+					{
+						id:1,
+						title: '信用卡丢了怎么补办？',
+						website: 'https://zhidao.baidu.com',
+						content: '持卡人应即时办理挂失。信用卡挂失后的盗用风险由银行承担，挂失前48小时内的非凭密码消费对河涸海干'
+					}
+				],
+				items: [
+					{ type: '', label: '标签一' },
+          { type: 'success', label: '标签二缓一缓' },
+          { type: 'info', label: '标签三哈巴河' },
+          { type: 'danger', label: '标签四' },
+          { type: 'warning', label: '标签五' }
+				]
+			}
+		},
+		filters: {
+			snippet: function(value) {
+        return value.slice(0,43) + "...";
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+/deep/.notes {
+	width: 440px;
+	height: 100%;
+	position: absolute;
+	left: 1000px;
+	.note_title {
+		line-height: 41px;
+		font-size: 12px;
+    color: #999999;
+		display: inline-block;
+		vertical-align: middle;
+	}
+	.note_content {
+		width: 336px;
+		height: 670px;
+		padding-left: 20px;
+	  padding-right: 22px;
+		background: #FFFFFF;
+    box-shadow: 0 2px 8px 0 rgba(0,0,0,0.05);
+	}
+	.explanation_title {
+		padding-top: 16px;
+		font-size: 18px;
+    color: #222222;
+	}
+	.explanation_content {
+		padding-top: 10px;
+		min-height: 20px;
+		font-size: 12px;
+    color: #666666;
+    letter-spacing: 0;
+    line-height: 20px;
+	}
+	.links {
+		height: 125px;
+		.el-divider--horizontal {
+			left: 0;
+		}
+		.links_title {
+			height: 19px;
+			font-size: 14px;
+			color: #222222;
+			letter-spacing: 0;
+		}
+		.links_website {
+			height: 16px;
+			font-size: 12px;
+			color: #999999;
+			letter-spacing: 0;
+		}
+		.links_content {
+			padding-top: 9px;
+			font-size: 12px;
+			color: #666666;
+			letter-spacing: 0;
+			line-height: 20px;
+		}
+	}
+	.share {
+		height: 114px;
+	}
+	.relevant_tags {
+
+	}
+}
+</style>
