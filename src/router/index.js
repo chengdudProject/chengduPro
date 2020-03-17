f3a49a28f96b2745689ed86420693225a5d20432
zhangrenjian <509634685@qq.com>
2020-03-17 17:49:37 +0800

    修改路由为懒加载路由，新增axios请求

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/view/HelloWorld'
-import search from '@/view/search'
-import searchContent from '@/view/searchContent'
-import contentRead from '@/view/contentRead'
-import mindMapping from '@/view/mindMapping'
-import relationNetwork from '@/view/relationNetwork'
+// import HelloWorld from '@/view/HelloWorld'
+// import search from '@/view/search'
+// import searchContent from '@/view/searchContent'
+// import contentRead from '@/view/contentRead'
+// import mindMapping from '@/view/mindMapping'
+// import relationNetwork from '@/view/relationNetwork'
 
 Vue.use(Router)
 
@@ -14,32 +14,32 @@ export default new Router({
     {
       path: '/',
       name: 'HelloWorld',
-      component: HelloWorld
+      component: ()=> import('@/view/HelloWorld')
     },
     {
       path: '/search',
       name: 'search',
-      component: search
+      component: ()=> import('@/view/search')
     },
     {
       path: '/searchContent/:searchVal',
       name: 'searchContent',
-      component: searchContent
+      component: ()=> import('@/view/searchContent')
     },
     {
       path: '/contentRead',
       name: 'contentRead',
-      component: contentRead
+      component: ()=> import('@/view/contentRead')
     },
     {
       path: '/mindMapping',
       name: 'mindMapping',
-      component: mindMapping
+      component: ()=> import('@/view/mindMapping')
     },
     {
       path: '/relationNetwork',
       name: 'relationNetwork',
-      component: relationNetwork
+      component: ()=> import('@/view/relationNetwork')
     }
   ]
 })
